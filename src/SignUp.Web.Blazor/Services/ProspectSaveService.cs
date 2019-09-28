da5dca925ff112b869f1d69d30f04390190389c0
Elton Stoneman <elton@sixeyed.com>
2019-09-28 20:29:16 +0100

    WIP - Blazor version of signup web

---
+++ src/SignUp.Web.Blazor/Services/ProspectSaveService.cs
@@ -0,0 +1,21 @@
+﻿using SignUp.Entities;
+using SignUp.Messaging;
+using SignUp.Messaging.Messages.Events;
+using System;
+
+namespace SignUp.Web.Blazor.Services
+{
+    public class ProspectSaveService 
+    {
+        public void SaveProspect(Prospect prospect)
+        {
+            var eventMessage = new ProspectSignedUpEvent
+            {
+                Prospect = prospect,
+                SignedUpAt = DateTime.UtcNow
+            };
+
+            MessageQueue.Publish(eventMessage);
+        }
+    }
+}
---
+++ src/SignUp.Web.Blazor/Services/ReferenceDataService.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+using SignUp.Core;
+using SignUp.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignUp.Web.Blazor.Services
+{
+    public class ReferenceDataService
+    {
+        public async Task<IEnumerable<Country>> GetCountriesAsync()
+        {
+            return new List<Country> {"A", "B", "C"};
+                        /*
+            var client = new RestClient(Config.Current["ReferenceDataApi:Url"]);
+            var request = new RestRequest("countries");
+            return await client.GetTaskAsync<List<Country>>(request);
+            */
+        }        
+
+        public async Task<IEnumerable<Role>> GetRoles()
+        {
+            return new List<Role> {"1", "2"};
+            /*
+            var client = new RestClient(Config.Current["ReferenceDataApi:Url"]);
+            var request = new RestRequest("roles");
+            return await client.GetTaskAsync<List<Role>>(request);
+            */
+        }
+    }
+}
