463e86e16991c18fd4401c0349ac453d000a8222
Elton Stoneman <elton@sixeyed.com>
2019-09-29 21:01:26 +0200

    Fixing .NET Core 3.0 refs

--- src/SignUp.Web.Blazor/Services/ReferenceDataService.cs
+++ src/SignUp.Web.Blazor/Services/ReferenceDataService.cs
@@ -10,22 +10,16 @@ namespace SignUp.Web.Blazor.Services
     {
         public async Task<IEnumerable<Country>> GetCountriesAsync()
         {
-            return new List<Country> {"A", "B", "C"};
-                        /*
             var client = new RestClient(Config.Current["ReferenceDataApi:Url"]);
             var request = new RestRequest("countries");
             return await client.GetTaskAsync<List<Country>>(request);
-            */
         }        
 
-        public async Task<IEnumerable<Role>> GetRoles()
+        public async Task<IEnumerable<Role>> GetRolesAsync()
         {
-            return new List<Role> {"1", "2"};
-            /*
             var client = new RestClient(Config.Current["ReferenceDataApi:Url"]);
             var request = new RestRequest("roles");
             return await client.GetTaskAsync<List<Role>>(request);
-            */
         }
     }
 }
