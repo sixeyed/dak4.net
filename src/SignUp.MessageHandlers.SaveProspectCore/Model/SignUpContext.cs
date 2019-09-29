63d7e911324b8c43de5332f010f06e3650b3cf1d
Elton Stoneman <elton@sixeyed.com>
2019-09-30 00:58:34 +0200

    Linux version fixes

--- src/SignUp.MessageHandlers.SaveProspectCore/Model/SignUpContext.cs
+++ src/SignUp.MessageHandlers.SaveProspectCore/Model/SignUpContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using SignUp.Entities;
 
@@ -9,6 +10,7 @@ namespace SignUp.MessageHandlers.SaveProspectCore.Model
         public SignUpContext(DbContextOptions options) : base(options)
         {
             Database.EnsureCreated();
+            SeedReferenceData();
         }
 
         public DbSet<Country> Countries { get; set; }
@@ -23,6 +25,63 @@ namespace SignUp.MessageHandlers.SaveProspectCore.Model
             builder.Entity<Role>().HasKey(r => r.RoleCode);
             builder.Entity<Prospect>().HasOne<Country>(p => p.Country);
             builder.Entity<Prospect>().HasOne<Role>(p => p.Role);
+        }
+
+        private void SeedReferenceData()
+        {
+            var seeded = false;
+            if (this.Roles.Count() == 0)
+            {
+                AddRole("-", "--Not telling");
+                AddRole("DA", "Developer Advocate");
+                AddRole("DM", "Decision Maker");
+                AddRole("AC", "Architect");
+                AddRole("EN", "Engineer");
+                AddRole("OP", "IT Ops");
+                seeded = true;
+            }
+
+            if (this.Countries.Count() == 0)
+            {
+                AddCountry("-", "--Not telling");
+                AddCountry("GBR", "United Kingdom");
+                AddCountry("USA", "United States");
+                AddCountry("PT", "Portugal");
+                AddCountry("NOR", "Norway");
+                AddCountry("SWE", "Sweden");
+                AddCountry("IRE", "Ireland");
+                AddCountry("DMK", "Denmark");            
+                AddCountry("IND", "India");
+                AddCountry("LIT", "Lithuania");
+                AddCountry("SPN", "Spain");
+                AddCountry("BGM", "Belgium");
+                AddCountry("TNL", "The Netherlands");            
+                AddCountry("POL", "Poland");
+                seeded = true;
+            }
+
+            if (seeded)
+            {
+                this.SaveChanges();
+            }
+        }
+
+        private void AddCountry(string code, string name)
+        {
+            this.Countries.Add(new Country
+            {
+                CountryCode = code,
+                CountryName = name
+            });
+        }
+
+        private void AddRole(string code, string name)
+        {
+            this.Roles.Add(new Role
+            {
+                RoleCode = code,
+                RoleName = name
+            });
         }
     }
 }
