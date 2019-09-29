f836b48066b88602c3251b65be536d34489194fc
Elton Stoneman <elton@sixeyed.com>
2019-09-29 21:10:55 +0000

    Finish Blazor signup page

--- src/SignUp.Web.Blazor/Startup.cs
+++ src/SignUp.Web.Blazor/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SignUp.Web.Blazor.Data;
 using SignUp.Web.Blazor.Services;
 
 namespace SignUp.Web.Blazor
@@ -23,17 +16,14 @@ namespace SignUp.Web.Blazor
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddServerSideBlazor();
+            services.AddServerSideBlazor().AddCircuitOptions(o => o.DetailedErrors = true);
             services.AddScoped<ReferenceDataService>();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<ProspectSaveService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -43,11 +33,8 @@ namespace SignUp.Web.Blazor
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
