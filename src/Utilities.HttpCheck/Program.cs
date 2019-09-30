846ea1e533b1e8d980a61085161fafadd39dd570
Elton Stoneman <elton@sixeyed.com>
2019-09-30 02:36:53 +0000

    Replace checks with .NET Core console apps

---
+++ src/Utilities.HttpCheck/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Utilities.HttpCheck
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var targetUrl = config["HttpCheck:TargetUrl"];
+            var timeout = int.Parse(config["HttpCheck:Timeout"]);
+
+            var exitCode = 1;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var stopwatch = Stopwatch.StartNew();
+                    var response = await client.GetAsync(targetUrl);
+                    stopwatch.Stop();
+                    Console.WriteLine($"HEALTHCHECK: status {response.StatusCode}, took {stopwatch.ElapsedMilliseconds}ms");
+                    if (response.StatusCode == HttpStatusCode.OK && stopwatch.ElapsedMilliseconds < timeout)
+                    {
+                        exitCode = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"HEALTHCHECK: error. Exception {ex.Message}");
+            }
+            return exitCode;
+        }
+    }
+}
---
+++ src/Utilties.DatabaseCheck/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Polly;
+using System;
+using System.Data.SqlClient;
+
+namespace Utilties.DatabaseCheck
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("DEPENDENCY: starting");
+
+            var sqlPolicy = Policy.Handle<SqlException>()
+                                  .Retry(3, (exception, retryCount) =>
+                                  {
+                                      Console.WriteLine("DEPENDENCY: Got SQL exception {0}, retryCount {1}", exception.GetType(), retryCount);
+                                  });
+
+            var result = sqlPolicy.ExecuteAndCapture(() => ConnectToSqlServer());
+            if (result.Outcome == OutcomeType.Successful)
+            {
+                Console.WriteLine("DEPENDENCY: OK");
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("DEPENDENCY: FAILED");
+                return 1;
+            }
+        }
+
+        private static void ConnectToSqlServer()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var targetDatabase = config["DatabaseCheck:DatabaseName"];
+            var timeout = int.Parse(config["DatabaseCheck:ConnectionTimeout"]);
+
+            var connectionString = config.GetConnectionString(targetDatabase);
+            var server = connectionString.Split(';')[0].Split('=')[1];
+            Console.WriteLine("DEPENDENCY: Connecting to SQL Server: {0}", server);
+            connectionString = $"{connectionString};Connect Timeout={timeout};";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+            }
+        }
+    }
+}
