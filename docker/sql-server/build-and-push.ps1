e6d847f3a463fd23fb1726a149d3bfe9703699b8
Elton Stoneman <elton@sixeyed.com>
2020-06-19 10:33:06 +0100

    Add Windows 2004 support

--- docker/sql-server/build-and-push.ps1
+++ docker/sql-server/build-and-push.ps1
@@ -1,6 +1,6 @@
 
 $image='dak4dotnet/sql-server:2017'
-$versions='ltsc2019','1903','1909'
+$versions='ltsc2019','1903','1909','2004'
 
 foreach($version in $versions) {
 
@@ -14,6 +14,7 @@ foreach($version in $versions) {
 docker manifest create --amend $image `
  "$($image)-$($versions[0])" `
  "$($image)-$($versions[1])" `
- "$($image)-$($versions[2])"
+ "$($image)-$($versions[2])" `
+ "$($image)-$($versions[3])"
  
 docker manifest push $image
